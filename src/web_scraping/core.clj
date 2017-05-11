4936be9ded92fdb4c4cdc6ab90dcefc258ae4c37
Paul Nguyen <paulng91@gmail.com>
2017-05-11 18:51:22 -0400

    Implemented separate attributes pulled from posts.

--- src/web_scraping/core.clj
+++ src/web_scraping/core.clj
@@ -2,13 +2,33 @@
   (:require [net.cgrand.enlive-html :as html]
             [org.httpkit.client :as http]))
 
+(def post-content
+  {:area-code nil
+   :phone-number nil
+   :reports nil
+   :content nil})
+
+
 (def ^:dynamic *base-url* "https://s3.us-east-2.amazonaws.com/gsd-auth-callinfo/callnotes.html")
 
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn get-posts []
-  (map html/text (html/select (fetch-url *base-url*) [:li.oos_listItem :div])))
+(defn posts []
+  (html/select (fetch-url *base-url*) [:div.oos_preview]))
+
+(defn area-code []
+  (into [] (map html/text (html/select (fetch-url *base-url*) [:div.oos_previewFooter :span :span]))))
+
+(defn phone-number []
+  (into [] (map html/text (html/select (fetch-url *base-url*) [:h4.oos_previewHeader]))))
+
+(defn amount-of-reports []
+  (into [] (map html/text (html/select (fetch-url *base-url*) [:div.oos_previewSide]))))
+
+(defn comment-contents []
+  (into [] (map html/text (html/select (fetch-url *base-url*) [:div.oos_previewBody]))))
+
 
 (defn get-dom
   []
